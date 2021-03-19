32c0dff49a7ff7603a49ff3b4daadfd1ee77a8a7
Louis Parent <contact@louis-parent.fr>
2021-03-19 22:41:03 +0100

    Adding test for parsing

---
+++ src/cox/test/TestXMLParsing.java
@@ -0,0 +1,71 @@
+package cox.test;
+
+import static jul.nla.Asserts.asserts;
+import static jul.nla.Asserts.not;
+import static jul.nla.Asserts.that;
+
+import cox.model.attribute.SimpleXMLAttribute;
+import cox.model.document.XMLDocument;
+import cox.model.element.XMLElement;
+import cox.model.element.XMLNodeElement;
+import cox.parser.COXParser;
+import cox.parser.exception.COXParserException;
+import jul.annotations.Test;
+import jul.annotations.TestSet;
+
+@TestSet
+public class TestXMLParsing
+{
+	@Test
+	public void testEmptyDocument() throws COXParserException
+	{
+		XMLDocument doc = COXParser.parse("");
+		asserts(that(doc.isEmpty()));
+	}
+
+	@Test
+	public void testNotEmptyDocument() throws COXParserException
+	{
+		XMLDocument doc = COXParser.parse("<root></root>");
+		asserts(not(that(doc.isEmpty())));
+	}
+	
+	@Test
+	public void testRootOnlyDocument() throws COXParserException
+	{
+		XMLDocument doc = COXParser.parse("<root></root>");
+		asserts(that(doc.getRoot().descendingEquals(new XMLNodeElement("root"))).isEqualTo(true));
+	}
+
+	@Test
+	public void testRootAndChildrenDocument() throws COXParserException
+	{
+		XMLDocument doc = COXParser.parse("<root><child1></child1><child2></child2></root>");
+		XMLElement root = new XMLNodeElement("root");
+		new XMLNodeElement("child1", root);
+		new XMLNodeElement("child2", root);
+
+		asserts(that(doc.getRoot().descendingEquals(root)).isEqualTo(true));
+	}
+
+	@Test
+	public void testRootAndAttributeDocument() throws COXParserException
+	{
+		XMLDocument doc = COXParser.parse("<root test=\"test\"></root>");
+		XMLElement root = new XMLNodeElement("root");
+		root.addAttribute(new SimpleXMLAttribute("test", "test"));
+
+		asserts(that(doc.getRoot()).isEqualTo(root));
+	}
+
+	@Test
+	public void testRootAndAttributesDocument() throws COXParserException
+	{
+		XMLDocument doc = COXParser.parse("<root test1=\"test1\" test2=\"test2\"></root>");
+		XMLElement root = new XMLNodeElement("root");
+		root.addAttribute(new SimpleXMLAttribute("test1", "test1"));
+		root.addAttribute(new SimpleXMLAttribute("test2", "test2"));
+
+		asserts(that(doc.getRoot()).isEqualTo(root));
+	}
+}
